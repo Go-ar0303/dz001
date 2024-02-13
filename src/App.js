0cdbe89535cd0bdb749df6d20df0f5f9b5ee3195
Go-ar0303 <gareginovna85@gmail.com>
2024-02-13 11:40:47 +0300

    project first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+    <button>Generate cat fact</button>
+    <p></p>
+    
+    </div>
+  );
+
+}
+
+
+export default App;
+
