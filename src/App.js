49e55c32f5d6ad2e32fc2925136da0885fa05429
Go-ar0303 <gareginovna85@gmail.com>
2024-02-13 20:55:12 +0300

    projec first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+
 
 function App() {
+//   fetch("https://catfact.ninja/fact")
+// .then((res) => res.json())
+// .then((data) =>{
+//   console.log(data);
+// });
+const [catFact, setCatFact] = useState("");
+const fetchCatFact = () =>
+{
+
+Axios.get("https://catfact.ninja/fact").then((res)=>
+{
+  setCatFact(res.data.fact);
+});
+};
+useEffect(() =>
+{
+  fetchCatFact();
+}, []);
 
   return (
     <div className="App">
-    <button>Generate cat fact</button>
-    <p></p>
+      <button className='btn' onClick={fetchCatFact}>Generate cat fact</button>
+      <p>{catFact}</p>
     
     </div>
   );
 
 }
 
-
-export default App;
 
